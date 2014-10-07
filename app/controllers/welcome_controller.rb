7dae37613c0a32da2f27c6819a1c0b7a2091c90b
Aaron Glasenapp <glassbead@gmail.com>
2014-10-07 11:25:35 -0600

    added webkit for responsive bg image

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     # Controller
-    @qr = RQRCode::QRCode.new( 'the QR code works', :size => 10, :level => :h )
+    @qr = RQRCode::QRCode.new( 'khanacademy.org', :size => 1, :level => :l )
   end
 end
